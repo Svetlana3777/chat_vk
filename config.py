0278f0b0e1e999254443eb8afd21ab2f728a40da
Светлана <115367183+Svetlana3777@users.noreply.github.com>
2023-07-18 22:40:05 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+community_token = 'vk1.a.Solxxxx'
+access_token = 'vk1.a.xxxxxxxTDZjv'
+
+db_url_object = "postgresql+psycopg2://student:student@localhost/student_diplom"
+
+host = '127.0.0.1'
+user = 'dfgjjkl'
+password = 'khfkjf'
+db_name = 'chat_bot_VK'
+port = '5432'
